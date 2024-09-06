b6f6d560e85dccddbe83de88b64b42f0eab47f73
Josh Garvin <84492192+joshgarv1n@users.noreply.github.com>
2024-09-06 15:50:46 -0500

    Add login & registration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './Home/Home'
+import Login from './Login/Login'
+import Registration from './Registration/Registration'
 import './App.css'
 
 const App: React.FC = () => {
@@ -11,6 +13,8 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Navigate to="/Home" replace />} />
         <Route path="/Home" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Registration" element={<Registration />} />
         {/* Add more routes here as you create more pages */}
       </Routes>
     </div>
