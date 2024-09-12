6a068290e6b1811f55d08ba31b264b04f8213d81
Josh Garvin <josh@Joshs-MBP-2.attlocal.net>
2024-09-12 13:51:51 -0500

    Add forgot username/password links

--- frontend/src/Login/Login.tsx
+++ frontend/src/Login/Login.tsx
@@ -16,16 +16,19 @@ const Login = () => {
         <h2 className="text-2xl font-bold mb-6 text-center">Welcome</h2>
 
         {/* Username Field */}
-        <div className="mb-4">
+        <div className="mb-1">
           <label className="block mb-2 text-sm font-bold text-gray-700 text-left">
             Username
           </label>
           <input 
             id="username"
             type="text"
-            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            className="w-full px-3 p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             placeholder="Enter your username"
           />
+          <div className="flex justify-end pt-1">
+            <a href="#" className="text-sm hover:underline">Forgot Username?</a>
+          </div>
         </div>
 
         {/* Password Field */}
@@ -40,10 +43,13 @@ const Login = () => {
             placeholder="Enter your password"
           />
           <div 
-            className="absolute inset-y-0 right-0 pr-3 pt-7 flex items-center cursor-pointer"
+            className="absolute inset-y-0 right-0 pr-3 pt-2 flex items-center cursor-pointer"
             onClick={togglePasswordVisibility}  // Click to toggle visibility
           >
             {showPassword ? <FaEyeSlash className="text-gray-500" /> : <FaEye className="text-gray-500" />} 
+          </div>
+          <div className="flex justify-end pt-1">
+            <a href="#" className="text-sm hover:underline">Forgot Password?</a>
           </div>
         </div>
 
@@ -60,7 +66,7 @@ const Login = () => {
           <p className="text-sm text-gray-600">
             Don't have a Volunteer account?
           </p>
-          <p className="text-blue-500 font-medium hover:underline">
+          <p className="text-blue-500 font-medium hover:underline hover:text-blue-600">
             <a href="#">Create an account</a>
           </p>
         </div>
