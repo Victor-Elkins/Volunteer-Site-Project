48842b3f8f7866bef2671e12b0b26c3a7884f7f2
Kenny Nguyen <ngkenny101@yahoo.com>
2024-10-16 16:08:37 -0500

    Fixing merging conflicts

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,9 @@ const cors = require('cors');
 const app = express();
 const notificationsRoutes = require('./routes/notifications');
 const historyRoutes = require('./routes/history');
+const authRoutes = require('./routes/auth');
 const eventsRoutes = require('./routes/events'); 
 const volunteerRoutes = require('./routes/volunteer'); 
-const authRoutes = require('./routes/auth');
 
 // Enable CORS for all routes
 app.use(cors({
@@ -26,6 +26,9 @@ app.use(session({
 // Routes
 app.use('/api/notifications', notificationsRoutes); // Notifications route
 app.use('/api/history', historyRoutes); // History route
+app.use('/api/auth', authRoutes); // Authorization route
+app.use('/api/events', eventsRoutes); // Event route
+app.use('/api/volunteer', volunteerRoutes) // Volunteer route
 
 const PORT = process.env.PORT || 5000; // Use PORT from environment or default to 5000
 app.listen(PORT, () => {
